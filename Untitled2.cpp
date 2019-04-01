6f0742ab3efcba27fe05d35756736c958750854c
hanificakmak <36795390+hanificakmak@users.noreply.github.com>
2019-04-02 02:52:55 +0300

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int  number,smallest,counter=1;
+    
+    printf("enter an integer:");
+    scanf("%d",&number);
+    
+    smallest=number;
+    
+    while(counter<=9)
+    {
+    
+	printf("enter an integer:");
+    scanf("%d",&number);
+    
+    if (number<smallest)
+    	
+    smallest=number;
+    
+    counter++;
+    	
+    	
+	}
+    
+    printf("smallest %d",smallest);
+    
+    return 0;
+}
