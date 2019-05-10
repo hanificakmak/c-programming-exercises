e7725357fabc6d35528ef56fcac7d234bef36104
hanificakmak <36795390+hanificakmak@users.noreply.github.com>
2019-05-10 12:16:39 +0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#define SIZE 5
+
+int main(){
+
+
+char s1[SIZE];
+char s2[SIZE];
+
+printf("Enter string1:");
+gets(s1);
+printf("Enter string2:");
+gets(s2);
+
+
+if(strcmp(s1,s2)>0)
+	printf("String1 > String2\n");
+	
+else if(strcmp(s1,s2)<0)
+	printf("String1 < String2\n");	
+
+else 
+	printf("String1 = String2\n");
+
+
+printf("%s",strcat(s1,s2));
+
+
+
+return 0;
+}
