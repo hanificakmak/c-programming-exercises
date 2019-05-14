360a58aa89d96cd30f62a5eda6ea227826bde70c
hanificakmak <36795390+hanificakmak@users.noreply.github.com>
2019-05-15 02:33:01 +0300

    Add files via upload

---
+++ asdada.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+int i;
+int a[5];
+int b[5];
+int element=0;
+
+
+	printf("Enter first array's element:");
+	scanf("%d %d %d %d %d",&a[0],&a[1],&a[2],&a[3],&a[4]);
+	
+	printf("Enter second array's element:");
+	scanf("%d %d %d %d %d",&b[0],&b[1],&b[2],&b[3],&b[4]);
+	
+for(i=0; i<=4; i++)
+{
+	if(a[i]>b[i])
+	{
+		element++;
+	}
+	
+}
+
+printf("%d elements in Array1 are greater than elements in Array2",element);
+
+return 0;
+}
+
