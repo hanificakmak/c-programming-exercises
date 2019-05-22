337b8541712b9fdd89165778e6a5a3e33aa68896
hanificakmak <36795390+hanificakmak@users.noreply.github.com>
2019-05-23 01:04:18 +0300

    Add files via upload

---
+++ 231.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+
+#define MAXCHARS 100
+
+char message[MAXCHARS];
+
+void convertToUpper(char []);
+
+printf("Type in any sequence of characters:\n");
+gets(message);
+
+//scanf("%s",message);
+
+convertToUpper(message);
+
+printf("The characters just entered, in uppercase are:\n%s\n",message);
+
+
+return 0;
+}
+
+void convertToUpper(char message[]){
+
+	int i;
+	
+	for(i=0 ; message[i]!='\0' ; i++)
+
+		message[i]=toupper(message[i]);
+
+}
